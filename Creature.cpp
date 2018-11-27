36e5b167bd80c85b504045cb78d106677d2605ca
zaqwer101 <platonov256@gmail.com>
2018-11-28 07:46:28 +0800

    Начал реализацию Creature

--- Creature.cpp
+++ Creature.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "Creature.h"
+
+Creature::Creature(std::string name)
+{
+    params["strength"]          = 1;
+    params["perception"]        = 1;
+    params["endurance"]         = 1;
+    params["charisma"]          = 1;
+    params["intelligence"]      = 1;
+    params["agility"]           = 1;
+    params["luck"]              = 1;
+    this->name = std::move(name);
+    recountStats();
+}
+
+std::string Creature::getName()
+{
+    return this->name;
+}
+
+Creature::Creature(std::string name, std::map<std::string, int> params)
+{
+    this->name = std::move(name);
+    this->params = std::move(params);
+    recountStats();
+}
+
+void Creature::recountStats()
+{
+    stats["max_hp"] = 10 + params["strength"] + params["endurance"] * 2 + stats["level"] * (params["endurance"] / 2 + 2);
+    stats["armor"] = params["strength"] * 0.1f;
+    rates["phys_resist"] = 1 - (0.03f * stats["armor"]) / (1 + 0.03f * stats["armor"]);
+
+    // Время между атаками todo После добавления оружия
+    // rates["attack_speed"] =
+}
--- Creature.h
+++ Creature.h
@@ -5,17 +5,29 @@
 #ifndef FINALGAME_CREATURE_H
 #define FINALGAME_CREATURE_H
 
+/*!
+ * Перечисление типов урона
+ */
+enum {
+
+    PHYSICAL, //!< Физический урон
+};
+
 #include<iostream>
 #include <map>
-
 class Creature
 {
 
 private:
-    std::string                     name;
-    std::map<std::string, float>    stats;  // хп, броня и остальное
-    std::map<std::string, int>      params; // сила, ловкость и остальное
-    std::map<std::string, float>    rates;  // блокирование брони, шанс уклонения и остальное
+    std::string                         name;
+    std::map<std::string, float>        stats;      // хп, броня и остальное
+    std::map<std::string, int>          params;     // сила, ловкость и остальное
+    std::map<std::string, float>        rates;      // блокирование брони, шанс уклонения и остальное
+
+    /*!
+     * @todo Составить список всех навыков и устанавливать их в конструкторе
+     */
+     std::map<std::string, float>    skills; // навыки существа
 
     /*!
      * Пересчитать параметры существа
@@ -29,6 +41,7 @@ public:
      * Дефолтный конструктор, при котором у существа будут минимальные параметры (1 для всех характеристик).
      */
     Creature(std::string name);
+
     Creature(std::string name, std::map<std::string, int> params);
 
     /*!
@@ -37,7 +50,7 @@ public:
      * @param amount Количество урона
      * @return Количество нанесённого урона
      */
-    float attack(std::string type, float amount);
+    float attack(int type, float amount);
 };
 
 
