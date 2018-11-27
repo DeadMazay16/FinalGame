637805cbf35ef4e1dbd1eb9705ba4a957d81b476
zaqwer101 <platonov256@gmail.com>
2018-11-28 06:00:08 +0800

    Базовое описание класса Creature

--- Creature.h
+++ Creature.h
@@ -15,7 +15,29 @@ private:
     std::string                     name;
     std::map<std::string, float>    stats;  // хп, броня и остальное
     std::map<std::string, int>      params; // сила, ловкость и остальное
-     
+    std::map<std::string, float>    rates;  // блокирование брони, шанс уклонения и остальное
+
+    /*!
+     * Пересчитать параметры существа
+     */
+    void recountStats();
+
+public:
+    std::string getName();
+
+    /*!
+     * Дефолтный конструктор, при котором у существа будут минимальные параметры (1 для всех характеристик).
+     */
+    Creature(std::string name);
+    Creature(std::string name, std::map<std::string, int> params);
+
+    /*!
+     * Атаковать данное существо
+     * @param type Тип наносимого урона
+     * @param amount Количество урона
+     * @return Количество нанесённого урона
+     */
+    float attack(std::string type, float amount);
 };
 
 
